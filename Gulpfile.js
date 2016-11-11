48f1323c75b469ffd2a5b934bae646e4b0cd4adc
Sam Richard <sam@snug.ug>
2016-11-11 09:24:55 -0800

    :new: Add Gulpfile
    We'll likely want to change this to something more reasonable; think about this a little more

---
+++ Gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const config = require('config');
+const runner = require('punchcard-runner');
+
+const options = runner.config({
+  application: {
+    library: {
+      src: [
+        'lib',
+        'config',
+        'content-types',
+        'input-plugins'
+      ],
+    }
+  },
+  tasks: {
+    nodemon: {
+      extension: 'js html yml'
+    },
+    build: {
+      clean: [
+        'clean:public',
+      ],
+      assets: [
+        'sass',
+        'imagemin',
+        'js',
+        'punchcard:js',
+        'punchcard:images'
+      ],
+    }
+  },
+  server: {
+    port: config.env.port,
+    host: config.env.host
+  }
+});
+
+runner(gulp, options);
+
+
+gulp.task('punchcard:js', () => {
+  return gulp.src('node_modules/punchcard-cms/src/js/**/*.js')
+    .pipe(concat('punchcard.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(path.join(options.assets._folders.public, options.assets.js.dest)));
+});
+
+gulp.task('punchcard:images', () => {
+  return gulp.src('node_modules/punchcard-cms/src/images/**/*')
+    .pipe(imagemin(options.tasks.images.build.options))
+    .pipe(gulp.dest(path.join(options.assets._folders.public, options.assets.images.dest)));
+});
