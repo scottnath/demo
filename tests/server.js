311840ef70d0e95d992ead7bab045363898d7902
Sam Richard <sam@snug.ug>
2016-11-11 09:26:09 -0800

    :white_check_mark: Add Tests

---
+++ tests/server.js
@@ -0,0 +1,25 @@
+import test from 'ava';
+import request from 'supertest';
+import punchcard from 'punchcard-cms';
+
+let agent;
+
+test.cb.before(t => {
+  punchcard().then(app => {
+    agent = request.agent(app);
+
+    t.end();
+  });
+});
+
+
+test.cb('CMS Landing Page', t => {
+  agent
+    .get('/login')
+    .expect(200)
+    .end((err, res) => {
+      t.is(err, null, 'Should not have an error');
+      t.regex(res.text, /DOCTYPE html/, 'Should have an HTML Doctype');
+      t.end();
+    });
+});
