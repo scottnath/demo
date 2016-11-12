abb1dd953a7f07aeee03fbfeca25f708383f4000
Sam Richard <sam@snug.ug>
2016-11-12 07:17:27 -0800

    :white_check_mark: Rearrange index.js to make it testable

--- index.js
+++ index.js
@@ -6,11 +6,24 @@
 const punchcard = require('punchcard-cms');
 const config = require('config');
 
-punchcard().then(app => {
-  app.listen(config.env.port, () => {
+const application = () => {
+  return punchcard();
+};
+
+/*
+  @description run the server if and only if this file is being run directly. Ignoring from code coverage as it's not possible to actually cover these lines.
+ */
+/* istanbul ignore next */
+if (!module.parent) {
+  application.then(app => {
+    app.listen(config.env.port, () => {
+      // Mean to console.log out, so disabling
+      console.log(`Server starting on ${config.env.url}`); // eslint-disable-line no-console
+    });
+  }).catch(e => {
     // Mean to console.log out, so disabling
-    console.log(`Server starting on ${config.env.url}`); // eslint-disable-line no-console
+    console.error(e.stack); // eslint-disable-line no-console
   });
-}).catch(e => {
-  console.log(e.stack);
-});
+}
+
+module.exports = application;
