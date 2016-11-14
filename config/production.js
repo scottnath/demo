037ef730bf9ae2c1a4cda53f32adf89bd4872520
Sam Richard <sam@snug.ug>
2016-11-14 11:07:13 -0500

    :bug: Use Heroku database connection

--- config/production.js
+++ config/production.js
@@ -1,23 +1,9 @@
 'use strict';
 
-const postgres = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  name: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-};
-
 module.exports = {
   knex: {
     dialect: 'pg',
-    connection: {
-      host: postgres.host,
-      user: postgres.user,
-      password: postgres.password,
-      database: postgres.name,
-      port: postgres.port,
-    },
+    connection: process.env.DATABASE_URL,
     debug: false,
     acquireConnectionTimeout: 5000,
   },
